e8ee654370537a76cac2193dd8443652844c6932
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-12-04 18:49:13 -0500

    created seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO role (id, burger_names, devoured) VALUES (1, 'monster burger' true)
+INSERT INTO role (id, burger_names, devoured) VALUES (2, 'veggie burger' false)
+INSERT INTO role (id, burger_names, devoured) VALUES (3, 'spicy burger', true)
