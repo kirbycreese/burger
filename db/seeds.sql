5f87a21d9248b793e01d15f8e33c7d5e02d3a74c
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2020-12-06 12:23:30 -0500

    created mysql connections through command line

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,5 @@
-INSERT INTO role (id, burger_names, devoured) VALUES (1, 'monster burger' true)
-INSERT INTO role (id, burger_names, devoured) VALUES (2, 'veggie burger' false)
-INSERT INTO role (id, burger_names, devoured) VALUES (3, 'spicy burger', true)
+USE burgers_db;
+
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'monster burger', true);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'veggie burger', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'spicy burger', true);
