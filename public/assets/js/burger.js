236f6fcdc48a055f6fb51b4cd054a8292a46e1d8
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-01-04 11:45:43 -0500

    updated pseudocode for burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,8 +1,10 @@
 //add new burger to waiting area on click
+//use POST method to add
 
 //move burger to devoured area on click
+//use PUT method to update
 
 //delete burger from waiting area
-
 //delete burger from devoured area
+//use DELETE to delete
         
